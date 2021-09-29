0d2003f51a7d945260024e8ab2a8ebfc112c4041
Matt Evans <38517811+matttevans@users.noreply.github.com>
2021-09-29 10:40:52 -0700

    Add Slide animation and code formatting

--- src/components/about.js
+++ src/components/about.js
@@ -2,46 +2,49 @@ import React from "react"
 import Fade from "react-reveal/Fade"
 import data from "../yourdata"
 import Bounce from "react-reveal/Bounce"
+import Slide from "react-reveal/Slide"
 
 const About = () => {
-  return (
-    <div className="secion" id="about">
-      <div className="container">
-        <div className="about-section">
-          <div className="content">
-            <Fade bottom cascade>
-              <h1>About Me</h1>
-            </Fade>
-            <p>
-              {data.aboutParaOne}
-              <br></br>
-              <br></br>
-              {data.aboutParaTwo}
-              <br></br>
-              <br></br>
-              {data.aboutParaThree}
-              <br></br>
-              <br></br>
-              {data.aboutParaFour}
-            </p>
-            <Fade bottom cascade>
-              <h1>Hobbies</h1>
-            </Fade>
-            <div className="hobby-icons">
-              {data.hobbies.map((hobby, index) => (
-                  <Bounce right>
-                    <img src={hobby.img} alt="icons"></img>
-                  </Bounce>
-              ))}
+    return (
+        <div className="secion" id="about">
+            <div className="container">
+                <div className="about-section">
+                    <div className="content">
+                        <Fade bottom cascade>
+                            <h1>About Me</h1>
+                        </Fade>
+                        <p>
+                            {data.aboutParaOne}
+                            <br></br>
+                            <br></br>
+                            {data.aboutParaTwo}
+                            <br></br>
+                            <br></br>
+                            {data.aboutParaThree}
+                            <br></br>
+                            <br></br>
+                            {data.aboutParaFour}
+                        </p>
+                        <Fade bottom cascade>
+                            <h1>Hobbies</h1>
+                        </Fade>
+                        <div className="hobby-icons">
+                            {data.hobbies.map((hobby, index) => (
+                                <Slide left>
+                                    <img src={hobby.img} alt="icons"></img>
+                                </Slide>
+                            ))}
+                        </div>
+                    </div>
+                    <Slide right>
+                        <div className="image-wrapper">
+                            <img src={data.aboutImage} alt="about"></img>
+                        </div>
+                    </Slide>
+                </div>
             </div>
-          </div>
-          <div className="image-wrapper">
-            <img src={data.aboutImage} alt="about"></img>
-          </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default About
